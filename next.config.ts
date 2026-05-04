f4eff97ac7f8fd0cb946db5898a072f62cf33f19
Muhammad Hizqil Alfi <Hizqilhizqil308@gmail.com>
2026-05-05 06:40:42 +0700

    fix: api_2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
